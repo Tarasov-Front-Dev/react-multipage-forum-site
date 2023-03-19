d452d3a7281f6e21fd9d2b1072c6dd2770c60f19
Tarasov-Front-Dev <tarasov.front.dev@gmail.com>
2023-03-19 08:16:10 +0000

    Return package.json, used Navigate comp

--- src/router/router.js
+++ src/router/router.js
@@ -3,14 +3,15 @@ import Posts from "../pages/Posts";
 import PostIdPage from "../pages/PostIdPage";
 import Error from "../pages/Error";
 import Login from "../pages/Login";
+import { Navigate } from "react-router-dom";
 
 export const privateRoutes = [
-  {path: '/', element: <Login/>},
+  {path: '/', element: <Navigate to='/posts' replace={true}/>},
   {path: '/about', element: <About/>},
   {path: '/posts', element: <Posts/>},
   {path: '/posts/:id', element: <PostIdPage/>},
-  {path: '/*', element: <Posts/>},
-  // {path: '/*', element: <Error/>},
+  // {path: '/*', element: <Posts/>},
+  {path: '/*', element: <Error/>},
 ]
 
 export const publicRoutes = [
