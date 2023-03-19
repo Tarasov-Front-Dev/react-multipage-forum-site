0b8542a08e7ef74a869cbea8df3ef6a6fa0d2c79
Tarasov-Front-Dev <tarasov.front.dev@gmail.com>
2023-03-19 07:51:41 +0000

    upd package.json

--- src/router/router.js
+++ src/router/router.js
@@ -4,19 +4,6 @@ import PostIdPage from "../pages/PostIdPage";
 import Error from "../pages/Error";
 import Login from "../pages/Login";
 
-// export const privateRoutes = [
-//   {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/', element: <Login/>},
-//   {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/about', element: <About/>},
-//   {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/posts', element: <Posts/>},
-//   {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/posts/:id', element: <PostIdPage/>},
-//   {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/*', element: <Error/>},
-// ]
-
-// export const publicRoutes = [
-//   {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/login', element: <Login/>},
-//   {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/*', element:<Login/>}
-// ]
-
 export const privateRoutes = [
   {path: '/', element: <Login/>},
   {path: '/about', element: <About/>},
