f707ac3233884839a07a532e1ce309003c30890b
Tarasov-Front-Dev <tarasov.front.dev@gmail.com>
2023-03-19 07:07:49 +0000

    Upd router

--- src/router/router.js
+++ src/router/router.js
@@ -5,15 +5,27 @@ import Error from "../pages/Error";
 import Login from "../pages/Login";
 
 export const privateRoutes = [
-  {path: 'tarasov-front-dev.github.io/', element: <Login/>},
-  {path: 'tarasov-front-dev.github.io/react-multipage-forum-site/', element: <Login/>},
-  {path: 'tarasov-front-dev.github.io/about', element: <About/>},
-  {path: 'tarasov-front-dev.github.io/posts', element: <Posts/>},
-  {path: 'tarasov-front-dev.github.io/posts/:id', element: <PostIdPage/>},
-  {path: 'tarasov-front-dev.github.io/*', element: <Error/>},
+  {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/', element: <Login/>},
+  {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/about', element: <About/>},
+  {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/posts', element: <Posts/>},
+  {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/posts/:id', element: <PostIdPage/>},
+  {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/*', element: <Error/>},
 ]
 
 export const publicRoutes = [
-  {path: 'tarasov-front-dev.github.io/login', element: <Login/>},
-  {path: 'tarasov-front-dev.github.io/*', element:<Login/>}
+  {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/login', element: <Login/>},
+  {path: 'https://tarasov-front-dev.github.io/react-multipage-forum-site/*', element:<Login/>}
 ]
+
+// export const privateRoutes = [
+//   {path: '/', element: <Login/>},
+//   {path: '/about', element: <About/>},
+//   {path: '/posts', element: <Posts/>},
+//   {path: '/posts/:id', element: <PostIdPage/>},
+//   {path: '/*', element: <Error/>},
+// ]
+
+// export const publicRoutes = [
+//   {path: '/login', element: <Login/>},
+//   {path: '/*', element:<Login/>}
+// ]
